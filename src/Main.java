fe4fa975ddcc4df720ba4bae999316733ea54b51
SoraHrS <soherreras@unal.edu.co>
2024-12-02 14:54:09 -0500

    se realizó el ejercicio 21

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        double lado1, lado2, lado3, perimetro, semiperimetro, area;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Ingrese el valor del lado 1 del triangulo: ");
+        lado1 = sc.nextDouble();
+        System.out.println("Ingrese el valor del lado 2 del triangulo: ");
+        lado2 = sc.nextDouble();
+        System.out.println("Ingrese el valor del lado 3 del triangulo: ");
+        lado3 = sc.nextDouble();
+        perimetro = lado1 + lado2 + lado3;
+        semiperimetro = perimetro / 2;
+        area = Math.sqrt(semiperimetro * (semiperimetro-lado1) * (semiperimetro-lado2) * (semiperimetro-lado3));
+        System.out.println("El perimetro del triangulo es: " + perimetro);
+        System.out.println("El semiperimetro del triangulo es: " + semiperimetro);
+        System.out.println("El area del triangulo es: " + area);
     }
 }
